d476544a922af55c70b40950213d0b2a7c283d09
Son Truong <sntruong12@gmail.com>
2019-04-15 16:18:30 -0500

    Updates

---
+++ precache-manifest.4d7e173d402df44e024cd200f712d32e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a89ff44736b22b916f1",
+    "url": "/unit-4-assessment/static/css/main.7940c4ae.chunk.css"
+  },
+  {
+    "revision": "2a89ff44736b22b916f1",
+    "url": "/unit-4-assessment/static/js/main.7d3f5495.chunk.js"
+  },
+  {
+    "revision": "b354662c43c1b1a188d7",
+    "url": "/unit-4-assessment/static/js/runtime~main.e55f4147.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/unit-4-assessment/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "74748b535479d9909fdb97d7163698d3",
+    "url": "/unit-4-assessment/index.html"
+  }
+];
